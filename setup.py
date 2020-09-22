dcd85685b36a93a72104f1a17136407f4fe1563f
Gage LeBlanc <gage.leblanc@optanix.com>
2020-09-21 22:27:32 -0400

    fixing deps

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name='huecli',
-    version='2.0.0',
+    version='2.0.1',
     scripts=['hue'],
     author="Gage LeBlanc",
     author_email="gleblanc@symnet.io",
@@ -15,7 +15,7 @@ setuptools.setup(
     url="https://github.com/gageleblanc/hue",
     packages=setuptools.find_packages(),
     install_requires=[
-        'clilib>=1.6.1',
+        'clilib>=1.8.0',
         'phue'
     ],
     classifiers=[
